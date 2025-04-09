aab9d1fd716db1efd5400ebb58095dda99836503
rdzPedraos <rdzpedraos@gmail.com>
2025-04-09 18:13:54 -0500

    fix: modify response when file is saved

--- src/lib/FileSystem.ts
+++ src/lib/FileSystem.ts
@@ -9,16 +9,16 @@ class FileSystem {
 
     static async save(path: string, data: any) {
         return new Promise((resolve, reject) => {
-            path = pathLib.join(this.getRootPath(), 'storage', path);
+            const fullPath = pathLib.join(this.getRootPath(), 'storage', path);
             const formattedData = JSON.stringify(data, null, 2);
 
-            fs.writeFile(path, formattedData, (err) => {
+            fs.writeFile(fullPath, formattedData, (err) => {
                 if (err) {
                     console.error('Error al guardar el archivo:', err);
                     reject(err);
                 }
 
-                console.log('Archivo guardado exitosamente:', path);
+                console.log(`Archivo guardado exitosamente: storage/${path}`);
                 resolve(path);
             });
         });
