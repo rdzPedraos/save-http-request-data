f6a5c62e195010666ec6a036d9b4786dbeb370dd
rdzPedraos <rdzpedraos@gmail.com>
2025-04-10 10:56:39 -0500

    feat: add method for set custom handle data

--- src/lib/HandleRequest.ts
+++ src/lib/HandleRequest.ts
@@ -1,8 +1,11 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import { FileSystem } from './FileSystem';
 
+type HandleDataType = (data: any, oldData: any) => any;
+
 class HandleRequest {
-    private data: any;
+    private data: any = {};
+    private handleData: HandleDataType;
     private promiseChain: Promise<any> = Promise.resolve();
 
     static create() {
@@ -13,24 +16,32 @@ class HandleRequest {
         this.promiseChain = this.promiseChain.then(callback);
     }
 
+    setHandleData(handleData: HandleDataType) {
+        this.handleData = handleData;
+        return this;
+    }
+
+    getData() {
+        return this.data;
+    }
+
     /**
      * Fetch data from a URL or an object with AxiosRequestConfig properties
      * @param props - URL or AxiosRequestConfig
      */
     fetch(props: AxiosRequestConfig | string) {
-        if (typeof props === 'string') {
-            props = { url: props };
-        }
-
         const callback = async () => {
-            try {
-                const response = await axios(props);
+            props = typeof props === 'string' ? { url: props } : props;
 
-                if (response.status < 200 || response.status >= 300) {
-                    throw new Error(response.statusText);
+            try {
+                const { status, data, statusText } = await axios(props);
+                console.log('Petición finalizada', status, statusText);
+
+                if (status < 200 || status >= 300) {
+                    throw new Error(statusText);
                 }
 
-                this.data = response.data;
+                this.data = this.handleData(data, this.getData());
             } catch (error) {
                 console.error('Error en la petición:', error);
                 throw error;
