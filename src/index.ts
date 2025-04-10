59e2141e1829e62be6c0fbab52f24c50646c44ed
rdzPedraos <rdzpedraos@gmail.com>
2025-04-10 10:22:07 -0500

    fix: set function for allow use await

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { HandleRequest } from '@/lib/HandleRequest';
 
-HandleRequest.create()
-    .fetch('https://jsonplaceholder.typicode.com/users')
-    .save('usuarios.json')
-    .execute()
-    .catch((error) => {
-        console.error('Ha ocurrido un error:', error.message);
-    });
+async function init() {
+    HandleRequest.create()
+        .fetch('https://jsonplaceholder.typicode.com/users')
+        .save('usuarios.json')
+        .execute()
+        .catch((error) => {
+            console.error('Ha ocurrido un error:', error.message);
+        });
+}
+
+init();
